867b0a0c4f9a934220c9a94fa662e375e536b358
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-01-12 11:36:46 +0100

    twelve

---
+++ index.js
@@ -0,0 +1,45 @@
+//1
+for (i=0; i<=10; i++)
+{if (i<=0) {console.log(true);} else {console.log(false);};}
+//2
+
+//3
+//4
+const month = ["january", "febraury", "march", "april", "may", "june", "july", "august", "aeptember", "october", "november", "december"];
+for(i=0; i < month.length; i++){console.log(month[0]);};
+//5
+const str = ("hi my name is ahmed, i") =>
+{let stra = str.split("");
+console.log(stra);
+
+
+
+
+//6
+const chr =["T"];
+let text3 = "";
+for (let i = 0; i < chr.length; i++){for(j=0; j<=4; j++){text3 += i + " ";}}
+
+const char =["*"];
+let text4 = "";
+for (let i = 0; i < char.length; i++){for(j=0; j<=4; j++){text4 += i + " ";}}
+
+const charc =["T"];
+let text5 = "";
+for (let i = 0; i < charc.length; i++){for(j=0; j<=4; j++){text5 += j + " ";}}
+
+
+
+//7
+let text1 ="";
+for (let i = 0; i <= 5; i++){
+for (let j = 1; j <= i; j++){text1 += j + " ";}}
+let text2 ="";
+for (let i = 0; i <= 5; i++){
+for (let j = 1; j <= i; j++){text2 += i + " ";}}
+//8
+const product = ["egg", "milk", "rice", "oranges", "bananas"];
+
+
+
+
